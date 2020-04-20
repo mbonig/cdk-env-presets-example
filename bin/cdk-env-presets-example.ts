5a36f61ac6e636062ad9edf0d4c9e50a507838b2
Matthew Bonig <matthew.bonig@gmail.com>
2020-04-20 12:42:55 -0600

    better example...

--- bin/cdk-env-presets-example.ts
+++ bin/cdk-env-presets-example.ts
@@ -6,7 +6,13 @@ import { CdkEnvPresetsExampleStack } from '../lib/cdk-env-presets-example-stack'
 const app = new cdk.App();
 
 const env_presets = app.node.tryGetContext("env_presets");
-const envPresets = require(`./${env_presets}.json`);
+if (!env_presets){
+    console.error("Please provide a context variable env_presets that points to a preset file in the ./environments directory.")
+    process.exit();
+}
+
+const envPresets = require(`./environments/${env_presets}.json`);
+
 new CdkEnvPresetsExampleStack(app, 'CdkEnvPresetsExampleStack', {
     envPresets
 });
--- lib/cdk-env-presets-example-stack.ts
+++ lib/cdk-env-presets-example-stack.ts
@@ -1,17 +1,25 @@
 import * as cdk from '@aws-cdk/core';
-import {CfnOutput, StackProps} from "@aws-cdk/core";
+import {CfnOutput, StackProps} from '@aws-cdk/core';
+import {Instance, InstanceClass, InstanceSize, InstanceType, MachineImage, Vpc} from "@aws-cdk/aws-ec2";
 
 
-export interface CdkEnvPresetsExampleStackProps extends StackProps{
-  envPresets: any;
+export interface CdkEnvPresetsExampleStackProps extends StackProps {
+    envPresets: { instanceType: string };
 }
-export class CdkEnvPresetsExampleStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: CdkEnvPresetsExampleStackProps) {
-    super(scope, id, props);
 
-    // The code that defines your stack goes here
-    new CfnOutput(this,  'some-usage-of-env-presets', {
-      value: props?.envPresets.instanceType
-    })
-  }
+export class CdkEnvPresetsExampleStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props: CdkEnvPresetsExampleStackProps) {
+        super(scope, id, props);
+
+        new Instance(this, 'some-instance', {
+            instanceType: InstanceType.of(props.envPresets.instanceType as InstanceClass, InstanceSize.MEDIUM),
+            machineImage: MachineImage.latestAmazonLinux(),
+            vpc: new Vpc(this, 'vpc')
+
+        });
+        // The code that defines your stack goes here
+        new CfnOutput(this, 'some-usage-of-env-presets', {
+            value: props?.envPresets.instanceType
+        })
+    }
 }
